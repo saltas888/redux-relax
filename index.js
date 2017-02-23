d654f1ccbdb2662c3ce0f5537995c7500dc7ba3e
Dimitris <d.saltaferis@gmail.com>
2017-02-24 01:06:45 +0200

    added hoc

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import { combineReducers } from 'redux'
+import { combineReducers, compose, applyMiddleware, createStore } from 'redux'
+import createSagaMiddleware, { END } from 'redux-saga'
 import Data from './data'
 import createReducers from './reducers'
 import merge from 'lodash/merge'
-import { createStore, applyMiddleware, compose } from 'redux'
+import * as ReduxRelaxCore from './core'
+import { fork } from 'redux-saga/effects'
+
 
 export default function initialize(configs){
   configs && Data.reinitialize(configs)
@@ -16,6 +19,28 @@ function initializeReducers(rootReducer) {
   return combineReducers({...createReducers(Data.configs.entities), ...rootReducer})
 }
 
-export function myCreateStore(rootReducer, ...args){
-  return createStore(initializeReducers(rootReducer),...args)
+export function myCreateStore(rootReducer,initialState, middlewares = [], tools = []){
+
+  // const store = createStore(initializeReducers(rootReducer),...args)
+  const sagaMiddleware = createSagaMiddleware()
+  console.log(middlewares, tools)
+  const store = createStore(
+    initializeReducers(rootReducer),
+    initialState,
+    compose(
+      applyMiddleware(
+        sagaMiddleware,
+        ...middlewares
+      ),
+      ...tools
+    )
+  )
+  store.runSaga = sagaMiddleware.run
+  store.close = () => store.dispatch(END)
+  store.runSaga(rootSaga)
+  return store
+}
+
+const rootSaga = function*() {
+  yield ReduxRelaxCore.getWatchers().map(watcher=>fork(watcher))
 }
--- loadDataHOC.js
+++ loadDataHOC.js
@@ -1,30 +1,48 @@
 import React from 'react'
 import connect from 'react-redux/lib/components/connect'
 import {bindActionCreators} from 'redux';
+import * as Utils from './utils'
+import get from 'lodash/get'
 
-export default (WrappedComponent,{entity}) => {
+export default (entity, dataRetriever) => {
 
-  return class extends React.Component {
+  return WrappedComponent =>{
+    class Connected extends React.Component {
 
-    static WrappedComponent = WrappedComponent;
+      static WrappedComponent = WrappedComponent;
 
-    render() {
-      return <WrappedComponent  {...this.props} />
+      shouldComponentUpdate(nextProps, nextState) {
+        if(nextProps[entity] !== this.props[entity] || nextProps.isFetching !== this.props.isFetching) return true
+        return false
+      }
+
+      componentWillMount() {
+        this.props[`load${Utils.capitalize(entity)}`](dataRetriever(this.props.state).search)
+      }
+
+      render() {
+        return <WrappedComponent  {...this.props} />
+      }
     }
+
+    function mapDispatchToProps(dispatch) {
+        return bindActionCreators({
+          [`load${Utils.capitalize(entity)}`]: query => Utils.action(`LOAD_${entity.toUpperCase()}`, {query}),
+          [`loadMore${Utils.capitalize(entity)}`]: query => Utils.action(`LOAD_MORE_${entity.toUpperCase()}`, {query})
+        }, dispatch);
+    }
+
+    function mapStateToProps(state) {
+      const entities = get(state,`entities.${entity}`)
+      const entityPaginationData = get(state,`pagination.${entity}.${dataRetriever(state).search}`) || { ids: [] }
+      const data = entityPaginationData.ids.map(id => entities[id])
+      return {
+        state,
+        [entity]:data,
+        isFetching:entityPaginationData.isFetching
+      }
+    }
+
+    return connect(mapStateToProps, mapDispatchToProps)(Connected);
   }
 }
-
-
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators({
-      //actions
-    }, dispatch);
-}
-
-function mapStateToProps(store) {
-    return {
-        //data
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Downloads);
